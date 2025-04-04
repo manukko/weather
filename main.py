1f7c13bf5950d3cf89d08c7c3cc2ac875b7ee34f
manukko <45536493+manukko@users.noreply.github.com>
2025-04-04 14:12:02 +0200

    Squashed commit of the following:
    commit 6d12b64aa09e1adb034b67771a65fb19cc3bd9e5
    Author: manukko <45536493+manukko@users.noreply.github.com>
    Date:   Fri Apr 4 14:11:10 2025 +0200
    
        add logic for prediction at date x
    
    commit 4deabecc4ebcbe21a524d966a9b29cd020e9a39a
    Author: manukko <45536493+manukko@users.noreply.github.com>
    Date:   Fri Apr 4 13:19:14 2025 +0200
    
        add forecasts endpoints to backend
    
    commit 741ffbd25e8fcfe524b59d53eda6bdb03839e3e6
    Author: manukko <45536493+manukko@users.noreply.github.com>
    Date:   Fri Apr 4 13:03:35 2025 +0200
    
        Create render.yaml
    
    commit 759bd75b557faba201044c9330f30f11b92ca38f
    Author: manukko <45536493+manukko@users.noreply.github.com>
    Date:   Fri Apr 4 12:55:55 2025 +0200
    
        Update main.py
    
    commit 8f8ae6c1a71e0f3236f6a60cddd4b7391349a407
    Author: manukko <45536493+manukko@users.noreply.github.com>
    Date:   Fri Apr 4 12:50:06 2025 +0200
    
        base logic
    
    commit 402cee0e7a049f0f2749bd050fa921a66df668f0
    Author: manukko <45536493+manukko@users.noreply.github.com>
    Date:   Fri Apr 4 12:49:58 2025 +0200
    
        Create requirements.txt
    
    commit 22033d01e5d6d901307b2e6c6b1dacda6b2b64f7
    Author: manukko <45536493+manukko@users.noreply.github.com>
    Date:   Fri Apr 4 11:41:54 2025 +0200
    
        Update .gitignore
    
    commit b72110785f0a9f957cccbd446acfb013726d3bae
    Author: manukko <45536493+manukko@users.noreply.github.com>
    Date:   Fri Apr 4 11:39:07 2025 +0200
    
        Update .gitignore

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import env
 import requests
 import fastapi
@@ -13,6 +14,9 @@ app = FastAPI(default_response_class=PrettyJSONResponse)
 
 API_KEY=env.API_KEY
 BASE_URL = "https://api.openweathermap.org"
+
+def _parse_date_str_to_ts(date_str):
+    return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S").timestamp()
 
 def _get_coordinates_by_location(location: str):
     location = location.lower().capitalize()
@@ -31,30 +35,57 @@ def _get_current_weather_by_coordinates(lon: float, lat: float):
         raise HTTPException(status_code=output.status_code, detail="Call to weather data service failed")
     return output.json()
 
-def _get_weather_forecast_by_location(lon: float, lat: float):
+def _get_weather_forecast_by_location(lon: float, lat: float, forecast_date_ts: int | None = None):
     output = requests.get(
     url=f"{BASE_URL}/data/2.5/forecast?lon={lon}&lat={lat}&units=metric&appid={API_KEY}"
     )
     if output.status_code != 200:
         raise HTTPException(status_code=output.status_code, detail="Call to weather data service failed")
-    return output.json()
+    if forecast_date_ts is not None:
+        desired_forecast = None
+        print(f"forecast_date_ts:{forecast_date_ts}")
+        for entry in output.json()["list"]:
+            print(entry["dt"])
+            if entry["dt"] < forecast_date_ts and entry["dt"] + 3*3600 > forecast_date_ts:
+                desired_forecast = entry
+                break
+        if not desired_forecast:
+            raise HTTPException(status_code=404, detail="Request date in the past or too far in the future")
+        json_output = output.json()
+        del json_output["list"]
+        json_output["forecast"] = desired_forecast
+    else:
+        json_output = output.json()
+    return json_output
 
 @app.get("/get_current_weather_by_coordinates")
-def get_weather_by_coordinates(lon: float = Query(title="longitude"), lat: float = Query(title="latitude")):
+def get_current_weather_by_coordinates(lon: float = Query(title="longitude"), lat: float = Query(title="latitude")):
     return _get_current_weather_by_coordinates(lon, lat)
 
 @app.get("/get_current_weather_by_location")
-def get_weather_by_location(location: str = Query(title="Geographical location")):
+def get_current_weather_by_location(location: str = Query(title="Geographical location")):
     lon, lat = _get_coordinates_by_location(location)
     return _get_current_weather_by_coordinates(lon, lat)
 
 
 @app.get("/get_weather_forecast_by_coordinates")
-def get_weather_forecast_by_coordinates(lon: float = Query(title="longitude"), lat: float = Query(title="latitude")):
-    return _get_weather_forecast_by_location(lon, lat)
+def get_weather_forecast_by_coordinates(lon: float = Query(title="longitude"), lat: float = Query(title="latitude"), forecast_date_string: str | None = Query(default=None, title="Date of the forecast in format YYYY-MM-DD hh:mm:ss", alias="date")):
+    forecast_date_ts = None
+    if forecast_date_string:
+        try:
+            forecast_date_ts = _parse_date_str_to_ts(forecast_date_string)
+        except:
+            raise HTTPException(status_code=400, detail="Invalid date format")
+    return _get_weather_forecast_by_location(lon, lat, forecast_date_ts)
 
 @app.get("/get_weather_forecast_by_location")
-def get_weather_forecast_by_location(location: str = Query(title="Geographical location")):
+def get_weather_forecast_by_location(location: str = Query(title="Geographical location"), forecast_date_string: str | None = Query(default=None, title="Date of the forecast in format YYYY-MM-DD hh:mm:ss", alias="date")):
+    forecast_date_ts = None
+    if forecast_date_string:
+        try:
+            forecast_date_ts = _parse_date_str_to_ts(forecast_date_string)
+        except:
+            raise HTTPException(status_code=400, detail="Invalid date format")
     lon, lat = _get_coordinates_by_location(location)
-    return _get_weather_forecast_by_location(lon, lat)
+    return _get_weather_forecast_by_location(lon, lat, forecast_date_ts)
 
